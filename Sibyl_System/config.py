02adb643a44f00004333a33a8a6b31ee7324dec2
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-04-01 18:08:35 +0530

    Update and rename sample_config.py to config.py

---
+++ Sibyl_System/config.py
@@ -0,0 +1,11 @@
+from typing import Union, Optional
+
+API_ID: int = 3498403
+API_HASH: str = "29f6f6a499ae994d6c6232f369dd45e7"
+STRING_SESSION: str = "1BVtsOG8BuxJzFjdKJftLbvfSypk7BA36JN4qfaBAL6npxc7IjJidvOloN1V-0qy9izowxc11graJtgRuwF1JNXcpI0btDn1XyIvnFJCYGd92ApXvw5-jBwz6vQqI-MF4bnBLo7aDG91UrCp6aWUDJhIHHHFPYNocBwB4JxeDdEJosrv2s0ndpSanaAJji1Z6oXyTJuT90YgzbshfrTHX7zO4THjy1KT1kPm_NPpsVQBCThJxxrX-CUk1HwoqVbSselgytfI2tyKowm9c7nTWvTOEEFrmr_S52-ZQajkmiRwSv4EBGaofT4vcRr-__MEnyTPTCbkNM3H8DR-2zffShUXQOUQBngk="
+Sibyl_logs: Union[str, int] = -1001161533311
+Sibyl_approved_logs: Union[str, int] = -1001207987353
+GBAN_MSG_LOGS: Optional[Union[str, int]] = None
+BOT_TOKEN: str = "1566032759:AAEEctQhHnPVPXdNYnHBYnexykkz0YopuxE"
+
+# Remove this line one you have read and understood the explanation of things in https://github.com/AnimeKaizoku/SibylSystem/blob/master/README.md
