2738a0def0858d8a7566bb58991a1678d518b59d
ryukmadox <81977502+ryukmadox@users.noreply.github.com>
2021-04-08 00:52:24 +0530

    Update config.py

--- Sibyl_System/config.py
+++ Sibyl_System/config.py
@@ -1,12 +1,12 @@
 from typing import Union, Optional
 
-API_ID: int = 3498403
-API_HASH: str = "29f6f6a499ae994d6c6232f369dd45e7"
-STRING_SESSION: str = "1BVtsOG8BuxJzFjdKJftLbvfSypk7BA36JN4qfaBAL6npxc7IjJidvOloN1V-0qy9izowxc11graJtgRuwF1JNXcpI0btDn1XyIvnFJCYGd92ApXvw5-jBwz6vQqI-MF4bnBLo7aDG91UrCp6aWUDJhIHHHFPYNocBwB4JxeDdEJosrv2s0ndpSanaAJji1Z6oXyTJuT90YgzbshfrTHX7zO4THjy1KT1kPm_NPpsVQBCThJxxrX-CUk1HwoqVbSselgytfI2tyKowm9c7nTWvTOEEFrmr_S52-ZQajkmiRwSv4EBGaofT4vcRr-__MEnyTPTCbkNM3H8DR-2zffShUXQOUQBngk="
-Sibyl_logs: Union[str, int] = -1001161533311
-Sibyl_approved_logs: Union[str, int] = -1001207987353
+API_ID: int = 3296233
+API_HASH: str = "b6315a1fc6d4d15d5278fea0c5a99c04"
+STRING_SESSION: str = "1BVtsOHcBu4oSCS5_Vfbnx8hR-eKBagqNuKIvGxVZCI4_f9KWQoFMOX47J7ZdkFgriwPQ8woPPCt83yPOxexGQdNNFu5bvl4OWTbPGl6MQgu739BD5MSGUs0Fc_LBwbWdhB4r7t7DhTyrLfqoU1hgBekEhF1ywqWs6KCwjiV5BliuHDp0UCMgbLi6s7PgNAM1w87WrNrfehhuXpImbIeD4ELhEXFqCJ7ix7KM7HUOX87ZXRVMGUatPVx3oCcYnxZPZnEXjm33TNKBYsHXvgwe2fQ_zmLKt6-qFAn0fLOUlA68T4lQksd7XZSyvN3DC5KXJCQy8-LhNGZE9NDIQfyGXK2JNZ9TNG8="
+Sibyl_logs: Union[str, int] = -569492198
+Sibyl_approved_logs: Union[str, int] = --562922694
 GBAN_MSG_LOGS: Optional[Union[str, int]] = None
-BOT_TOKEN: str = "1566032759:AAEEctQhHnPVPXdNYnHBYnexykkz0YopuxE"
+BOT_TOKEN: str = "1729271071:AAFECOUzUuQU0jo8PFDUpBwHeGYk7JcJngM"
 MONGO_DB_URL = "mongodb+srv://tg-god-usr:xPpLPNSgvBB2OWvk@cluster0.em0bd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 # Remove this line one you have read and understood the explanation of things in https://github.com/AnimeKaizoku/SibylSystem/blob/master/README.md
